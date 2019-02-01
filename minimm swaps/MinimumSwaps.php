66ca87e127033790d0725d394ac0e52799d5df5a
abdelrahman badr <abdelrahmanbadr@abdelrahmans-MacBook-Pro-2.local>
2019-02-01 21:47:40 +0200

    WIP

--- minimm swaps/MinimumSwaps.php
+++ minimm swaps/MinimumSwaps.php
@@ -2,31 +2,31 @@
 
 class  MinimumSwaps
 {
-    function minimumSwaps($arr)
+
+
+    public function minimumSwaps($arr)
     {
-        $indexArray = [];
-        $sortedArray = $arr;
-        $count = 1;
-        sort($sortedArray);
-        foreach ($arr as $position => $element) {
-            if (isset($indexArray[$element])) {
-
-                $count++;
+        $swaps = 0;
+        for ($i = 0; $i < count($arr); $i++) {
+            if ($i + 1 != $arr[$i]) {
+                $t = $i;
+                while ($arr[$t] != $i + 1) {
+                    $t++;
+                }
+                $temp = $arr[$t];
+                $arr[$t] = $arr[$i];
+                $arr[$i] = $temp;
+                $swaps++;
             }
-            $indexArray[$element] = $position;
         }
-
-        foreach ($sortedArray as $key => $val) {
-
-            if ($key == $indexArray[$val]) {
-                $count++;
-            }
-
-        }
-
-        return count($arr) - $count;
+        return $swaps;
     }
+
 }
+
+
+//var_dump(minimumSwaps([2, 31, 1, 38, 29, 5, 44, 6, 12, 18, 39, 9, 48, 49, 13, 11, 7, 27, 14, 33, 50, 21, 46, 23, 15, 26, 8, 47, 40, 3, 32, 22, 34, 42, 16, 41, 24, 10, 4, 28, 36, 30, 37, 35, 20, 17, 45, 43, 25, 19])); // right ==3
+
 //minimumSwaps([7, 1, 3, 2, 4, 5, 6]);
 //var_dump(minimumSwaps([9,1,2,6,5])); // right ==3
 //var_dump(minimumSwaps([7, 1, 3, 2, 4, 5, 6])); // right ==5
